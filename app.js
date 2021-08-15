ca5e1abdcb90a57af20cfa8e6d5edd734a818961
arpit-20 <arpit.ghai@franconnect.com>
2021-08-15 22:19:26 +0530

    improvement

--- app.js
+++ app.js
@@ -40,7 +40,7 @@ if(process.env.NODE_ENV == "production"){
     app.use(express.static("Shockmechanica/dist"));
     const path=require("path");
         app.get("*",(req,res)=>{
-            res.sendFile(path.resolve('server','Shockmechanica','dist','index.html'));
+            res.sendFile(path.resolve(_dirname,'Shockmechanica','dist','index.html'));
         })
 }
 
