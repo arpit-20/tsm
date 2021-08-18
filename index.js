a53ebebd9266b9ac66e865eb5a01dd2e5ec477e8
arpit-20 <arpit.ghai@franconnect.com>
2021-08-18 11:34:59 +0530

    data improvement

--- index.js
+++ index.js
@@ -15,18 +15,19 @@ const Port=process.env.PORT || 3000
 const userRouter=require('./routes/userRoutes');
 
 const customerRouter=require('./routes/customerRoutes');
-
+const path=require("path");
 app.use('/user',userRouter);
 
 app.use('/customer',customerRouter);
 
-if(process.env.NODE_ENV == "production"){
-    app.use(express.static("Shockmechanica/dist"));
-    const path=require("path");
-        app.get("*",(req,res)=>{
-            res.sendFile(path.resolve('server','Shockmechanica','dist','index.html'));
+// if(process.env.NODE_ENV == "production"){
+    app.use(express.static(path.join(__dirname,'Shockmechanica','dist','Shockmechanica')));
+    console.log("-->",__dirname);
+ 
+        app.get("/*",(req,res)=>{
+            res.sendFile(path.join(__dirname,'Shockmechanica','dist','Shockmechanica','index.html'));
         })
-}
+// }
 
 mongoose.connect('mongodb+srv://TSM123:uTfXYXKjnYBVPO0x@cluster0.kzcr0.mongodb.net/TSM?retryWrites=true&w=majority',
 {
@@ -47,6 +48,6 @@ mongoose.set('useCreateIndex', true)
 
 
 app.listen((process.env.PORT || 3000),()=>{
-    console.log('server started.... @ localhost://3000')
+    console.log('server started.... @ localhost://'+process.env.PORT)
     
 })
